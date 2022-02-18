4bcce11b4073a1217f6a0134edfa9ad23985e755
Ubuntu <ubuntu@ip-172-31-49-187.ec2.internal>
2022-02-18 04:15:55 +0000

    created a hello world with header comments.

---
+++ HelloWorld.swift
@@ -0,0 +1,11 @@
+//
+// HelloWorld.swift
+//
+// created by Ketia Gaelle Kaze
+// created on 2022-02-17
+// version 1.0
+// Copyright (c) 2022 Ketia Gaelle Kaze. All rights reserved.
+//
+// Created a hello world program that displays "Hello,World!"
+// as an output to the terminal.
+print("Hello,World!")
